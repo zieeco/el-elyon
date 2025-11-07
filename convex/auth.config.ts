ed6d6ab4bc630c1e64c0f450d9def235693d7fb8
zieeco <zieecosam@gmail.com>
2025-11-07 14:36:15 +0100

    feat: update to use clerk auth

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,8 +1,8 @@
 export default {
-  providers: [
-    {
-      domain: process.env.CONVEX_SITE_URL,
-      applicationID: "convex",
-    },
-  ],
+	providers: [
+		{
+			domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
+			applicationID: 'convex',
+		},
+	],
 };

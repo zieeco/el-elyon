1a0e2308740d39220b46f5e128cdc62ef54b79b4
zieeco <zieecosam@gmail.com>
2025-11-10 12:39:53 +0100

    remove convex reference

--- eslint.config.js
+++ eslint.config.js
@@ -1,77 +1,71 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  {
-    ignores: [
-      "dist",
-      "eslint.config.js",
-      "convex/_generated",
-      "postcss.config.js",
-      "tailwind.config.js",
-      "vite.config.ts",
-    ],
-  },
-  {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommendedTypeChecked,
-    ],
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
-      parserOptions: {
-        project: [
-          "./tsconfig.node.json",
-          "./tsconfig.app.json",
-          "./convex/tsconfig.json",
-        ],
-      },
-    },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-      // All of these overrides ease getting into
-      // TypeScript, and can be removed for stricter
-      // linting down the line.
+	{
+		ignores: [
+			'dist',
+			'drizzle',
+			'node_modules',
+			'eslint.config.js',
+			'postcss.config.js',
+			'tailwind.config.js',
+			'vite.config.ts',
+			'drizzle.config.ts',
+		],
+	},
+	{
+		extends: [
+			js.configs.recommended,
+			...tseslint.configs.recommendedTypeChecked,
+		],
+		files: ['**/*.{ts,tsx}'],
+		languageOptions: {
+			ecmaVersion: 2020,
+			globals: {
+				...globals.browser,
+				...globals.node,
+			},
+			parserOptions: {
+				project: ['./tsconfig.node.json', './tsconfig.app.json'],
+			},
+		},
+		plugins: {
+			'react-hooks': reactHooks,
+			'react-refresh': reactRefresh,
+		},
+		rules: {
+			...reactHooks.configs.recommended.rules,
+			'react-refresh/only-export-components': [
+				'warn',
+				{allowConstantExport: true},
+			],
 
-      // Only warn on unused variables, and ignore variables starting with `_`
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        { varsIgnorePattern: "^_", argsIgnorePattern: "^_" },
-      ],
+			// Only warn on unused variables, and ignore variables starting with `_`
+			'@typescript-eslint/no-unused-vars': [
+				'warn',
+				{varsIgnorePattern: '^_', argsIgnorePattern: '^_'},
+			],
 
-      // Allow escaping the compiler
-      "@typescript-eslint/ban-ts-comment": "error",
+			// Allow escaping the compiler
+			'@typescript-eslint/ban-ts-comment': 'error',
 
-      // Allow explicit `any`s
-      "@typescript-eslint/no-explicit-any": "off",
+			// Allow explicit `any`s
+			'@typescript-eslint/no-explicit-any': 'off',
 
-      // START: Allow implicit `any`s
-      "@typescript-eslint/no-unsafe-argument": "off",
-      "@typescript-eslint/no-unsafe-assignment": "off",
-      "@typescript-eslint/no-unsafe-call": "off",
-      "@typescript-eslint/no-unsafe-member-access": "off",
-      "@typescript-eslint/no-unsafe-return": "off",
-      // END: Allow implicit `any`s
+			// START: Allow implicit `any`s
+			'@typescript-eslint/no-unsafe-argument': 'off',
+			'@typescript-eslint/no-unsafe-assignment': 'off',
+			'@typescript-eslint/no-unsafe-call': 'off',
+			'@typescript-eslint/no-unsafe-member-access': 'off',
+			'@typescript-eslint/no-unsafe-return': 'off',
+			// END: Allow implicit `any`s
 
-      // Allow async functions without await
-      // for consistency (esp. Convex `handler`s)
-      "@typescript-eslint/require-await": "off",
-    },
-  },
+			// Allow async functions without await for consistency
+			'@typescript-eslint/require-await': 'off',
+		},
+	}
 );

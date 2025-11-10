162913712a79be548fddeb1e0c4472fdaf081e21
zieeco <zieecosam@gmail.com>
2025-11-10 12:56:55 +0100

    update import

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,4 @@
-import {drizzle} from 'drizzle-orm/neon-serverless';
+import {drizzle} from 'drizzle-orm/neon-http';
 import {neon} from '@neondatabase/serverless';
 
 const sql = neon(process.env.DATABASE_URL!);
